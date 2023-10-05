b2346c6ea4d56bd164359ea399f0497ee60e4de9
BetaPatrik <144523892+BetaPatrik@users.noreply.github.com>
2023-10-05 14:09:59 +0300

    WSL Setup

--- InstallWSL.bat
+++ InstallWSL.bat
@@ -1,7 +1,7 @@
 @title WSL Setup
 @echo Do You Want To Install WSL?
 @c:\windows\system32\choice.exe /c:ny
-@if errorlevel 2 (wsl.exe --install --web-download --no-launch)
+@if errorlevel 2 (wsl.exe --install --web-download)
 @if errorlevel 1 (exit /b)
 @echo Windows Subsystem For Linux Is Installed
 @pause
